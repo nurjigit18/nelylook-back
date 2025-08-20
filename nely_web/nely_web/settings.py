e74e5bf23bfdd1e7837a9b5ce897b6bc7df5b184
nurjigit18 <nurzhigitkurmanov@gmail,com>
2025-08-20 16:29:30 +0600

    whitenoise conf

--- nely_web/nely_web/settings.py
+++ nely_web/nely_web/settings.py
@@ -142,7 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
