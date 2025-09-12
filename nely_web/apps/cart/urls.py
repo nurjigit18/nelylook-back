32d9c6dcfa392e82f31d626ef212f65c1528f366
nurjigit18 <nurzhigitkurmanov@gmail,com>
2025-09-12 17:49:58 +0600

    duplicate urls

--- nely_web/apps/cart/urls.py
+++ nely_web/apps/cart/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 from .views import CartViewSet
 
 router = SimpleRouter()
-router.register(r"cart", CartViewSet, basename="cart")
+router.register(r"", CartViewSet, basename="cart")
 
 urlpatterns = [
     path("", include(router.urls)),
